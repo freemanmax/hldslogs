f07b77b9ffd59c85baf04bb6237fa906192e6a22
freemanmax <nenadstojkovikj@gmail.com>
2016-04-19 05:47:46 +0200

    Gem created

---
+++ lib/hldslogs.rb
@@ -0,0 +1,8 @@
+require "hldslogs/version"
+require "socket"
+require "hldslogs/hldssocket"
+
+
+module Hldslogs
+  # Your code goes here...
+end
---
+++ lib/hldslogs/version.rb
@@ -0,0 +1,3 @@
+module Hldslogs
+  VERSION = "0.2.0"
+end
