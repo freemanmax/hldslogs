38f1af1badfa7d6abca0084ceb2e6e293edd02e2
freemanmax <nenadstojkovikj@gmail.com>
2016-04-20 05:51:00 +0200

    Add method socket

--- lib/hldslogs/hldssocket.rb
+++ lib/hldslogs/hldssocket.rb
@@ -1,23 +1,26 @@
 # encoding: US-ASCII
 require "socket"
 
-
-  class HldsSocket
-    def initialize(ip_addr, port, bind_port)
-       @socket = UDPSocket.new
-       @socket.bind "", bind_port
-       @socket.connect ip_addr, port
-    end
-    def close
-       @socket.close
-       return true
-    end
-    def receve_data       
-       paket = @socket.recvfrom(1400)
-       data = paket[0]
-       data.gsub!("\xFF\xFF\xFF\xFFlog ", "")
-       data.gsub!("\n\x00", "")
-    end
+class HldsSocket
+  def initialize(ip_addr, port, bind_port)
+     @socket = UDPSocket.new
+     @socket.bind "", bind_port
+     @socket.connect ip_addr, port
   end
+  def close
+     @socket.close
+     @socket = nil
+     return true
+  end
+  def receve_data       
+     paket = @socket.recvfrom(1400)
+     data = paket[0]
+     data.gsub!("\xFF\xFF\xFF\xFFlog ", "")
+     data.gsub!("\n\x00", "")
+  end
+  def socket
+     @socket
+  end
+end
 
 
